9d5659e2aa7177157669670ff45b13c73a49559b
EnzoMShiotuqui <RM551508@fiap.com.br>
2023-04-11 11:45:48 -0300

    mudei aq

---
+++ js/zyb-5647.js
@@ -0,0 +1,17 @@
+// const liElement= document.getElementsByTagName("li")
+
+// for (let index = 0; index < liElement.length; index++) {
+//    console.log(liElement[index].textContent) 
+// }
+
+
+//declaração de arrays
+
+let nr1 = [1,2,3,4,5]
+let nr2 = [6,7,8,9,10]
+let nr3 = [nr1,nr2]
+
+console.log(`Arrays-1: ${nr1}`)
+console.log(`array-2: ${nr2}`)
+
+console.log(`array-3:${nr3}`)
