45b97b5b5d95b2f74bc7d1c57a22c35dde8d0357
enzomshiotuqui <RM551508@fiap.com.br>
2023-04-18 11:39:10 -0300

    estudando função

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,39 +1,44 @@
 
-// for (let index = 0; index < liElement.length; index++) {
-//    console.log(liElement[index].textContent) 
-// }
+// // for (let index = 0; index < liElement.length; index++) {
+// //    console.log(liElement[index].textContent) 
+// // }
 
 
-//declaração de arrays
+// //declaração de arrays
 
-let nr1 = [1,2,3,4,5]
-let nr2 = [6,7,8,9,10]
-let nr3 = [nr1,nr2]
+// let nr1 = [1,2,3,4,5]
+// let nr2 = [6,7,8,9,10]
+// let nr3 = [nr1,nr2]
 
-console.log(`Arrays-1: ${nr1}`)
-console.log(`array-2: ${nr2}`)
+// console.log(`Arrays-1: ${nr1}`)
+// console.log(`array-2: ${nr2}`)
 
-console.log(`array-3:${nr3}`)
+// console.log(`array-3:${nr3}`)
 
-// nr3.forEach((itemDoaArray) =>{
-//     console.log(`Array-3 sendo listado ${itemDoaArray}`)
+// // nr3.forEach((itemDoaArray) =>{
+// //     console.log(`Array-3 sendo listado ${itemDoaArray}`)
+// // })
+
+// nr3.map((numero, key)=>{
+
+//     console.log(`${(key+1)} Itens do array- ${numero}`)
 // })
 
-nr3.map((numero, key)=>{
 
-    console.log(`${(key+1)} Itens do array- ${numero}`)
-})
-
-
-//convertendo Html em array
-const liElementHTML = document.getElementsByTagName("li")
-const liElementArray = [...liElementHTML]
+// //convertendo Html em array
+// const liElementHTML = document.getElementsByTagName("li")
+// const liElementArray = [...liElementHTML]
 
 
 
-liElementArray.map((item, key)=>{
-    if (item.textContent == "item-39"){
-        console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
-    }
-})
+// liElementArray.map((item, key)=>{
+//     if (item.textContent == "item-39"){
+//         console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
+//     }
+// })
 
+function soma(a,b){
+    console.log(`O resultado será ${a + b}`)
+
+}
+soma(23, 43)
