3364bb7f8d34c797361258a4cb376e31c90c5ebe
EnzoMShiotuqui <RM551508@fiap.com.br>
2023-04-25 10:58:12 -0300

    estudando

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,53 +1,84 @@
 
-// // for (let index = 0; index < liElement.length; index++) {
-// //    console.log(liElement[index].textContent) 
-// // }
+// // // for (let index = 0; index < liElement.length; index++) {
+// // //    console.log(liElement[index].textContent) 
+// // // }
 
 
-// //declaração de arrays
+// // //declaração de arrays
 
-// let nr1 = [1,2,3,4,5]
-// let nr2 = [6,7,8,9,10]
-// let nr3 = [nr1,nr2]
+// // let nr1 = [1,2,3,4,5]
+// // let nr2 = [6,7,8,9,10]
+// // let nr3 = [nr1,nr2]
 
-// console.log(`Arrays-1: ${nr1}`)
-// console.log(`array-2: ${nr2}`)
+// // console.log(`Arrays-1: ${nr1}`)
+// // console.log(`array-2: ${nr2}`)
 
-// console.log(`array-3:${nr3}`)
+// // console.log(`array-3:${nr3}`)
 
-// // nr3.forEach((itemDoaArray) =>{
-// //     console.log(`Array-3 sendo listado ${itemDoaArray}`)
+// // // nr3.forEach((itemDoaArray) =>{
+// // //     console.log(`Array-3 sendo listado ${itemDoaArray}`)
+// // // })
+
+// // nr3.map((numero, key)=>{
+
+// //     console.log(`${(key+1)} Itens do array- ${numero}`)
 // // })
 
-// nr3.map((numero, key)=>{
 
-//     console.log(`${(key+1)} Itens do array- ${numero}`)
-// })
-
-
-// //convertendo Html em array
-// const liElementHTML = document.getElementsByTagName("li")
-// const liElementArray = [...liElementHTML]
+// // //convertendo Html em array
+// // const liElementHTML = document.getElementsByTagName("li")
+// // const liElementArray = [...liElementHTML]
 
 
 
-// liElementArray.map((item, key)=>{
-//     if (item.textContent == "item-39"){
-//         console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
-//     }
-// })
+// // liElementArray.map((item, key)=>{
+// //     if (item.textContent == "item-39"){
+// //         console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
+// //     }
+// // })
 
-// function soma(a,b){
-//     console.log(`O resultado será ${a + b}`)
+// // function soma(a,b){
+// //     console.log(`O resultado será ${a + b}`)
 
+// // }
+// // soma(23, 43)
+
+
+// function soma(p1,p2,p3) {
+//     nr1 = p1;
+//     nr2 = p2;
+//     resultado = parseInt(nr1) + parseInt(nr2) + parseInt(p3)
+//     console.log(`O Resultado é : ${resultado}.`)
+//     return (resultado)
+//     // console.log(`Resultado : ${parseInt(nr1) + parseInt(nr2) + parseInt(parametro)}`);
+//     // console.log(`Este é o parâmetro que foi passado : ${parametro}`);
 // }
-// soma(23, 43)
 
-
-//arrow function
-const mudaCor = () =>{
-    //função settimeout
-    setTimeout(alert("executou"),1000)
-}
-muda()
+// //ARROW FUNCTION
+// const mudaCor = ()=>{
     
+//     //FUNÇÃO SET-TIME-OUT
+//     setTimeout(soma, 5000, 100,100,100);
+// }
+
+// mudaCor()
+
+
+function mudaCor(){
+
+    let r = ""
+    let g = ""
+    let b = ""
+
+    //math - random= que gera números aleatórios entre 0 e 1 
+    //ceil, floor e round que arredondam o número para cima, para baixo ou aleatóriamente  
+    
+    //determinando para a variavel R um valor entre 0 e 255
+    r = Math.round(Math.random() * 255) 
+    g = Math.round(Math.random() * 255) 
+    b = Math.round(Math.random() * 255) 
+    
+    const cabecalho = document.querySelector(".cabecalho")
+    cabecalho.setAttribute("style", `background-color:rgb( ${r}, ${g}, ${b})`)
+}
+mudaCor()
